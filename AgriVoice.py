db8d761e9d93337ed86b472c1c33cd19d102bb3b
Madhu M <madhumardoor30@gmail.com>
2025-02-26 00:54:54 +0530

    Update AgriVoice.py

--- AgriVoice.py
+++ AgriVoice.py
@@ -44,7 +44,7 @@ st.markdown(
         white-space: nowrap; /* Prevents text wrapping */
         overflow: hidden;
         text-overflow: ellipsis;
-        color: white; /* Adjust color based on background */
+        color: transparent; /* Adjust color based on background */
     }
     </style>
     <p class="title">🌾 AGRIVOICE: Upload, Translate & Listen Instantly! 🌏🔊</p>

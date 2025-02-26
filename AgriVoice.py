f514ad379a6f2fd81e01d41e3af1c10e55ff3fbe
Madhu M <madhumardoor30@gmail.com>
2025-02-26 23:16:51 +0530

    Update AgriVoice.py

--- AgriVoice.py
+++ AgriVoice.py
@@ -104,7 +104,7 @@ if uploaded_file:
 
     # Display extracted text
     st.subheader("📜 Extracted Text")
-    st.write(policy_text[])  
+    st.write(policy_text)  
 
     # ------------------- Clean Extracted Text -------------------
     def clean_text(text):
